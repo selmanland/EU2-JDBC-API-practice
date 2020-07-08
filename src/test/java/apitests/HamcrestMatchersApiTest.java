284a83e0289f42d9bcc327a3781b02d40f6427d1
hiltas73 <h_iltas@yahoo.com>
2020-07-08 07:39:28 +0300

    08_07 07.15

--- src/test/java/apitests/HamcrestMatchersApiTest.java
+++ src/test/java/apitests/HamcrestMatchersApiTest.java
@@ -24,13 +24,13 @@ public class HamcrestMatchersApiTest {
     @Test
     public void OneSpartanwithHamcrest(){
         given().accept(ContentType.JSON)
-                .and().pathParam("id", 15)
-                .and().auth().basic("admin", "admin").
-                when().get("http://3.81.99.109:8000/api/spartans/{id}")
-                .then().statusCode(200)
-                .assertThat().contentType("application/json;charset=UTF-8")
-                .and().assertThat().body("id", equalTo(15),
-                "name",equalTo("Meta"),
+                .and().pathParam("id", 15)      //search by id=15 send request
+                .and().auth().basic("admin", "admin")
+                .when().get("http://3.81.99.109:8000/api/spartans/{id}")
+                .then().statusCode(200)                 //assert Status Code is 200
+                .assertThat().contentType("application/json;charset=UTF-8")     //assert Content Type
+                .and().assertThat().body("id", equalTo(15),       //assert about BODY now...
+                "name",equalTo("Meta"),          //body works with Hamcrest Matchers
                 "gender",equalTo("Female"),
                 "phone",equalTo(1938695106));
 
@@ -56,12 +56,10 @@ public class HamcrestMatchersApiTest {
     public void TeacherWithdepartments(){
 
         given().accept(ContentType.JSON).and()
-                .pathParam("name","Computer")
+                .pathParam("name","Computer")   //send request by Dept name=Comp
                 .when().get("http://api.cybertektraining.com/teacher/department/{name}")
                 .then().statusCode(200).and().contentType("application/json;charset=UTF-8")
                 .and().assertThat().body("teachers.firstName", hasItems("Alexander","tvrec","Marteen"));
-
-
 
     }
 
